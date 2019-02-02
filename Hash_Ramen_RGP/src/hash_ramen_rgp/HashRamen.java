780eaa8d97849665061407ba0016622a23d125f4
Ricardo Guevara <ricardogp@protonmail.com>
2019-02-02 09:50:16 -0500

    dis

---
+++ Hash_Ramen_RGP/src/hash_ramen_rgp/HashRamen.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hash_ramen_rgp;
+
+/**
+ *
+ * @author RICARDO
+ */
+public class HashRamen {
+    
+    private TablaHash[] ramen;  //grupo de tablas del ramen
+    private int     densidad,   //densidad: número de tablas en el mapa = número de campos por registro
+                    capacidad;  //capacidad: espac´o o campos disponibles de la tabla
+    private String  separador;  //caracter de separacion de cada registro
+    
+    public HashRamen(int densidad, int capacidad, String separador_registros) 
+    {
+        this.capacidad = capacidad;
+        this.densidad = densidad;
+        this.separador = separador_registros;
+        ramen = new TablaHash[densidad];    //numero de tablas
+        for (int i = 0; i < densidad; i++) {
+            ramen[i] = new TablaHash(capacidad);
+        }
+    }
+    
+    public void print()
+    {
+        for (TablaHash tablaHash : ramen) {
+            System.out.println("__________________________Ramen");
+            tablaHash.printTable();
+        }
+    }
+
+    public void insert(String reg)
+    {
+        insert(separar(reg));
+    }
+    
+    public void insert(CampoRegistro reg) 
+    {
+        for (TablaHash tablaHash : ramen) 
+        {
+            tablaHash.add(reg);
+            reg = reg.getDer();
+        }
+    }
+    
+    public CampoRegistro separar(String reg)
+    {
+        CampoRegistro   cpa = null, //campo de registro actual
+                        cpc = null; //campo de cabecera
+        
+        for (String campo : reg.split(this.separador)) 
+        {
+            if (cpa != null)
+            {
+                cpa.addChain(new CampoRegistro(campo));
+                cpa = cpa.getDer();
+            }
+            else
+            {
+                cpa = new CampoRegistro(campo);
+                cpc = cpa;
+            }
+        }
+        return cpc;
+    }
+    
+    public int getDensidad() {
+        return densidad;
+    }
+
+    public void setDensidad(int densidad) {
+        this.densidad = densidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    
+    
+    
+    
+}
---
+++ Hash_Ramen_RGP/src/hash_ramen_rgp/hash_ramen_rgp.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hash_ramen_rgp;
+
+/**
+ *
+ * @author rpayan
+ */
+public class hash_ramen_rgp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println(new Registro("aaa;bbb;ccc"));
+        /*
+        TablaHash th = new TablaHash(10);
+        
+        th.add(new CampoRegistro("aaa;bbb;ccc"));
+        th.add(new CampoRegistro("ddd;eee;fff"));
+        th.add(new CampoRegistro("ggg;hhh;iii"));
+        th.add(new CampoRegistro("aaa;ste;otro"));
+        th.add(new CampoRegistro("aaa;ste;ultimo"));
+        th.printTable();
+        System.out.println("search:\n");
+        System.out.println(th.search("aaa;bbb;ccc"));
+        */
+        
+        HashRamen ramen = new HashRamen(3,10,";");
+        ramen.insert("aaa;bbb;ccc");
+        ramen.insert("ddd;bbb;fff");
+        ramen.insert("ggg;hhh;fff");
+        ramen.print();
+    }
+    
+    
+    
+}
