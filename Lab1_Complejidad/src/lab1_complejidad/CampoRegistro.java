7c1a578b34da1b4506620463f83ce5106b82b203
Ricaro Guevara <ricardogp@protonmail.com>
2019-02-02 00:37:16 -0500

    hash-ramen corregido (estructura)

---
+++ Lab1_Complejidad/src/lab1_complejidad/CampoRegistro.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_complejidad;
+
+/**
+ *
+ * @author RICARDO
+ */
+public class CampoRegistro {
+
+    private String          campo;  //valor/texto del campo
+    private CampoRegistro   der,    //siguiente campo
+                            izq,    //campo anterior
+                            down;    //siguiente en cadena de hash
+
+    public CampoRegistro(String campo) {
+        this.campo = campo;
+    }
+    
+    @Override
+    public String toString()
+    {
+        printRegistChain();
+        return "";
+    }
+    
+    public void addBlok(CampoRegistro cp)
+    {
+        if (this.down==null) 
+            this.down = cp;
+        else
+            this.down.addBlok(cp);
+    }
+
+    public void addChain(CampoRegistro cp)
+    {
+        if (this.der==null)
+        { 
+            this.der = cp;
+            cp.izq = this;
+        }
+        else
+            this.der.addChain(cp);
+    }
+    
+    public void printBlok()
+    {
+        this.printRegistChain();
+        if(this.down!=null)
+            this.down.printBlok();
+    }
+    
+    public void printRegistChain() 
+    {
+        System.out.print("--> ");
+        getStartPoint().printNexts();
+        System.out.println("");
+    }
+        
+    public void printNexts()
+    {
+        System.out.print(this.campo);
+        if (this.der!=null) 
+        {
+            System.out.print(",");
+            this.der.printNexts();
+        }
+    }
+    
+    public CampoRegistro getStartPoint()
+    {
+        if(this.izq==null)
+            return this;
+        else
+            return this.izq.getStartPoint();
+    }
+    
+    public String getCampo() {
+        return campo;
+    }
+
+    public CampoRegistro getDer() {
+        return der;
+    }
+
+    public void setDer(CampoRegistro der) {
+        this.der = der;
+    }
+
+    public CampoRegistro getIzq() {
+        return izq;
+    }
+
+    public void setIzq(CampoRegistro izq) {
+        this.izq = izq;
+    }
+
+    public CampoRegistro getSig() {
+        return down;
+    }
+
+    public void setSig(CampoRegistro sig) {
+        this.down = sig;
+    }
+
+    
+    
+    
+    
+}
---
+++ Lab1_Complejidad/src/lab1_complejidad/TablaHash.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_complejidad;
+
+/**
+ *
+ * @author rpayan
+ */
+public class TablaHash {
+    
+    private CampoRegistro tabla[];
+
+    public TablaHash(int capacidad_tabla) {
+        tabla = new CampoRegistro[capacidad_tabla];
+    }
+    
+    public void add(CampoRegistro registro)
+    {
+        int hash = hash(registro);
+        if (this.tabla[hash]==null)
+            this.tabla[hash]=registro;
+        else
+            this.tabla[hash].addBlok(registro);
+    }
+    
+    public CampoRegistro search(String clave)
+    {
+        return this.tabla[hash(clave)];
+    }
+    
+    private int hash(CampoRegistro cr)
+    {
+        return hash(cr.getCampo());
+    }
+    
+    private int hash(String key)
+    {
+        int sum=0;
+        for (char let : key.toCharArray()) {
+            sum += (int)let;
+        }
+        return (sum<tabla.length)?tabla.length % sum:sum%tabla.length;
+    }
+    
+    public void printTable()
+    {
+        for (CampoRegistro registro : tabla) {
+            //System.out.println(registro);
+            if (registro==null)
+                System.out.println("null");
+            else
+                //registro.printRegistChain();
+                registro.printBlok();
+            System.out.println("_____");
+        }
+    }
+    
+}
